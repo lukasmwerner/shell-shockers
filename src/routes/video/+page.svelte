72af8e3e717d7ab6bed5270fcf677539f667710e
Lukas Werner <lukas.m.werner@outlook.com>
2024-04-24 14:33:26 -0600

    video

---
+++ src/routes/video/+page.svelte
@@ -0,0 +1,17 @@
+<style>
+:root {
+	margin: 0;
+	padding: 0;
+	background: black;
+}
+video {
+	height: 100vh;
+	width: 100vw;
+}
+</style>
+<script>
+import vid from '$lib/assembly.mp4';
+</script>
+<video autoplay loop controls>
+	<source src={vid} type="video/mp4" />
+</video>

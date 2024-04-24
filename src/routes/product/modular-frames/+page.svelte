19ac28d05c5b88659a664c9dde76f761e3f90ddb
Lukas Werner <lukas.m.werner@outlook.com>
2024-04-24 14:35:03 -0600

    switch to new list products system instead of cost

--- src/routes/product/modular-frames/+page.svelte
+++ src/routes/product/modular-frames/+page.svelte
@@ -5,6 +5,15 @@
 	let title = "Modular Picture Frames";
 	let description = `Ea sunt id adipisicing. Ad et elit ullamco deserunt anim laborum aute magna ex ex aliqua et ut officia. Dolor velit dolor quis occaecat deserunt. Officia enim cupidatat proident et aute incididunt commodo aute velit cupidatat ipsum officia exercitation ad. Excepteur esse excepteur ut culpa proident elit eu eiusmod laboris nisi cillum eiusmod veniam deserunt consectetur.
 Proident ullamco aliqua Lorem velit occaecat occaecat culpa nisi dolor velit. Pariatur tempor sit labore cillum cillum amet amet. Eu enim adipisicing nisi id ea non eiusmod tempor dolore officia. Officia duis exercitation pariatur consequat officia commodo anim Lorem voluptate. Id fugiat irure incididunt ex nisi magna in esse velit aliqua id eiusmod sint.`;
-	let cost = "$0.10/ct";
+
+	let products = [
+		{name: "Acrylic", dynamic: true, cost: "$0.00/ct", costFunction: (w, h) => 0.03*(w*h) },
+		{name: "Frame Backing", dynamic: true, cost: "$0.00/ct", costFunction: (w, h) => 0.09*(w*h) + 0.23 },
+		{name: "Hanging System", cost: "$5.50/ct"},
+		{name: "Frame Side", cost: " $0.54/ct"},
+		{name: "Frame Corner", cost: "$2.56/ct"},
+		{name: "Snap on Frame Side Borders", cost: "$0.14/ct"},
+		{name: "Snap on Frame Corner Borders", cost: "$0.66/ct"},
+	]
 </script>
-<ProductPage images={[one, two]} {cost} {title} {description} />
+<ProductPage images={[one, two]} {products} {title} {description} />
